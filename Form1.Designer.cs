38565e6ce5afa5f1dd63e381d344647aa1ae4dd5
oxgerrero <bojacasa@gmail.com>
2022-10-21 18:00:13 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace brazoRobot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button_1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Button_1
+            // 
+            this.Button_1.AutoSize = true;
+            this.Button_1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Button_1.ForeColor = System.Drawing.Color.Black;
+            this.Button_1.Location = new System.Drawing.Point(46, 29);
+            this.Button_1.Name = "Button_1";
+            this.Button_1.Size = new System.Drawing.Size(75, 30);
+            this.Button_1.TabIndex = 0;
+            this.Button_1.Text = "base I";
+            this.Button_1.UseVisualStyleBackColor = false;
+            this.Button_1.Click += new System.EventHandler(this.RBi_Click);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(127, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "base D";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(46, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Hombro up";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(166, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Hombro d";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Location = new System.Drawing.Point(513, 142);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 30);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Mano cerrar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Location = new System.Drawing.Point(371, 142);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 30);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "MAno abrir";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.Location = new System.Drawing.Point(486, 78);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(102, 30);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Codo Down";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.AutoSize = true;
+            this.button7.Location = new System.Drawing.Point(371, 78);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(82, 30);
+            this.button7.TabIndex = 4;
+            this.button7.Text = "Codo Up";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Button_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button_1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace brazoRobot
+{
+    public partial class Form1 : Form
+    {
+        System.IO.Ports.SerialPort Arduino;
+        string rotarBaseIzquierda = "";
+        string rotarBaseDerecha = "";
+        string rotarHombroArriba = "";
+        string rotarHombroAbajo = "";
+        string rotarCodoArriba = "";
+        string rotarCodoAbajo = "";
+        string rotarManoAbrir = "";
+        string rotarManoCerrar = "";
+        public Form1()
+        {
+            InitializeComponent();
+            abrirPuerto();
+        }
+
+        private void abrirPuerto()
+        {
+            Arduino = new System.IO.Ports.SerialPort();
+            Arduino.PortName = "COM5";
+            Arduino.BaudRate = 9600;
+            Arduino.Open();
+        }
+
+        //rotar base a la izquierda
+        private void RBi_Click(object sender, EventArgs e)
+        {
+            Arduino.WriteLine(rotarBaseIzquierda);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Arduino.WriteLine(rotarBaseDerecha);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Arduino.WriteLine(rotarHombroArriba);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Arduino.WriteLine(rotarHombroAbajo);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Arduino.WriteLine(rotarCodoArriba);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Arduino.WriteLine(rotarCodoAbajo);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Arduino.WriteLine(rotarManoAbrir);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Arduino.WriteLine(rotarManoCerrar);
+        }
+    }
+}
